0583643d8dac85d3ec83b8900b43065d22c22d2a
Travis Wessman <traviswessman26@gmail.com>
2025-06-20 21:58:39 -0400

    new new

--- temp.py
+++ temp.py
@@ -7,13 +7,26 @@ import time
 
 s = Service(r"C:\My Projects\Advanced Web Scraping\msedgedriver.exe")
 driver = webdriver.Edge(service=s)
-driver.get("https://new.dineoncampus.com/umassd/whats-on-the-menu")
+driver.get("https://new.dineoncampus.com/umassd/whats-on-the-menu/the-grove/2025-06-25/lunch")
+# wait for some time to load the website
 time.sleep(10)
 
-html = driver.page_source
+button_click = driver.find_element(By.XPATH,"/html/body/div/div/div/main/div[1]/div[3]/div/div[2]/div[1]/div[2]/div[2]/table/tbody/tr[1]/td[1]/div[1]/span")
+button_click.click()
+time.sleep(5)
 
-path = 'new.html'
 
+temp = driver.find_element(By.XPATH, "/html/body/div/div/div/main/div[2]/div/div/div")
 
-with open(path, 'w', encoding='utf-8') as f:
-    f.write(html)
+text = temp.text
+print(text)
+
+print("Text saved to output.txt successfully!")
+
+time.sleep(5)
+
+# close the nutrition information window
+close_button = driver.find_element(By.XPATH, "/html/body/div/div/div/main/div[2]/div/button[1]").click()
+time.sleep(5)
+
+driver.quit()
