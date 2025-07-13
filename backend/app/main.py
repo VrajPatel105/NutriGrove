c675dc7baee8e8bf28bff1400ee6d50a4d2eed0c
Vraj Patel <vrajp1052005@gmail.com>
2025-07-13 03:51:47 -0400

    database connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from scraper import Scraper  # Import your Scraper class
 from datetime import datetime
-from data.clean_data import clean_data
+from data.clean_data import FoodDataCleaner
+from database import SupabaseUploader
 
 # Option 1: Use today's date
 today_date = datetime.today().strftime('%Y-%m-%d')
@@ -8,14 +9,20 @@ scraper = Scraper(today_date)
 
 try:
     
-    scraper.fetch_breakfast()
-    scraper.fetch_lunch()
-    # scraper.fetch_brunch()
-    scraper.fetch_dinner()
-    print("All data scraped successfully!")
-    # cleaning the scraped data
-    clean_data.clean_data_func()
-    print("All data cleaned")
+    # scraper.fetch_breakfast()
+    # scraper.fetch_lunch()
+    # # scraper.fetch_brunch()
+    # scraper.fetch_dinner()
+    # print("All data scraped successfully!")
+    # # cleaning the scraped data
+    # FoodDataCleaner.clean_food_data()
+    # print("All data cleaned")
+
+    # Create uploader instance and upload
+    uploader = SupabaseUploader()
+    uploader.upload_json_file('backend/app/data/cleaned_data/all_food_items_cleaned.json')
+    print('Data uploaded to database')
+
     
 finally:
     scraper.close()
