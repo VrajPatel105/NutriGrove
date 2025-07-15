4ae21e2a3905c050203d34401062b6ee84f74cf3
Vraj Patel <vrajp1052005@gmail.com>
2025-07-15 01:44:43 -0400

    few changes

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,9 +1,19 @@
 # Using fastapi for getting response and sending resopnses to the user
 from fastapi import FASTAPI
 from fastapi.responses import JSONResponse
+from model.schema import UserInput
 
 app = FASTAPI()
 
-@app.get('/recommendations')
-def get_recommendations():
-    
+@app.post('/recommendations')
+def recommendations(data: UserInput):
+    user_preferences = {
+        'age': data.age,
+        'weight': data.wegith,
+        'height': data.height,
+        'dietary_restrictions': data.dietary_restrictions,
+        'calories' : data.calories,
+        'protein': data.protein,
+        'comments': data.comments
+    }
+
--- backend/app/model/schema.py
+++ backend/app/model/schema.py
@@ -5,19 +5,12 @@ class UserInput(BaseModel):
     age: Annotated[int,Field(...,gt=0, lt=100, description='Age of the User')]
     wegith: Annotated[int, Field(..., gt=0, description='Weight of the user in lbs')]
     height: Annotated[int, Field(..., gt=50, description='Height of the User in centimeters (cm)')]
-    dietary_restrictions: Annotated[str, Field()]
-    calories: Annotated[int, Field(...,)]
+    dietary_restrictions: Annotated[str, Field(...,description='All the restrictions from the user')]
+    calories: Annotated[int, Field(...,gt=0, description="Calories a user is expecting to consume in grams")]
+    protein: Annotated[int, Field(...,gt=0, description="Protein a user is expecting to consume in grams")]
+    comments: Annotated[str,Field(description="Any additional comments from the user")]
 
-
-
-
-# user_preferences = {
-#     "dietary_restrictions": ["vegetarian"],
-#     "allergies": ["nuts"],
-#     "calorie_target": 2500,
-#     "protein_target": 120,
-#     "weight_kg": 64 ,
-#     "height_cm": 177,
-#     "age": 20,
-#     "comments":"I dont want fried food today. ALso today i am really craving for some good salad :) . also, i am indian vegeterian meaning that i dont eat eggs too. so no eggs please"
-# }
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        return self.weight/(self.height**2)
